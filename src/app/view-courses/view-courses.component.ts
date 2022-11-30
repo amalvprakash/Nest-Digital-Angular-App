a58e89db4fa93ba19c2790b97b5a495d21f4cd56
Amal <amalprakash2025@gmail.com>
2022-11-30 21:35:28 +0530

    Added view courses

---
+++ src/app/view-courses/view-courses.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
+
+@Component({
+  selector: 'app-view-courses',
+  templateUrl: './view-courses.component.html',
+  styleUrls: ['./view-courses.component.css']
+})
+export class ViewCoursesComponent {
+  constructor(private api:ApiService){
+    api.viewCourses().subscribe(
+      (response)=>{
+        this.courses = response;
+      }
+    )
+  }
+courses:any = {}
+}
