a1bac6a1a5d63a7030d813f2695f35e66bbd1453
Amal <amalprakash2025@gmail.com>
2022-11-30 12:44:48 +0530

    added homePage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePAgeComponent } from './home-page/home-page.component';
 
+const myRoute:Routes =[
+  {
+    path:"",
+    component:HomePAgeComponent
+  }
+  ]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePAgeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
