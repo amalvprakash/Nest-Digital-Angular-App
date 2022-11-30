a497e17b77e7b74faf91150186d1f300bcb7b241
Amal <amalprakash2025@gmail.com>
2022-11-30 14:46:37 +0530

    Admin login, navigation

---
+++ src/app/admin-login/admin-login.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-admin-login',
+  templateUrl: './admin-login.component.html',
+  styleUrls: ['./admin-login.component.css']
+})
+export class AdminLoginComponent {
+  username = ""
+  password = ""
+
+  constructor(private route:Router){}
+
+  login = () => {
+    let data:any = {
+      "username":this.username,
+      "password":this.password
+    }
+    if (this.username == "admin" && this.password == "nestdigital") {
+      alert("Login Successfull\b Welcome Admin")
+      this.route.navigate(["/adminHome"])
+
+    } else {
+      alert("Login Failed!!!")
+      this.username = ""
+      this.password = ""
+    }
+  }
+}
