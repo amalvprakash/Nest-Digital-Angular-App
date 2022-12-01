f2d93f4c4a05647245029a2478aca3e3cd5845f0
Amal <amalprakash2025@gmail.com>
2022-12-01 09:20:49 +0530

    debug

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact-us.component.css']
 })
 export class ContactUsComponent {
-
+  name =""
+  email = ""
+  compony =""
+  number = ""
+  subject =""
+  contact = () =>{
+    let data:any = {
+      "name":this.name,
+      "email":this.email,
+      "compony":this.compony,
+      "number":this.number,
+      "subject":this.subject
+    }
+    console.log(data)
+    this.name =""
+    this.email = ""
+    this.compony =""
+    this.number = ""
+    this.subject =""
+  }
 }
